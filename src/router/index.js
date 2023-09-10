3b3d9dd7ba8ce158f70fd9585302ef2e5a05d8d9
molul <luismorcillo@gmail.com>
2023-09-10 18:07:40 +0200

    Clicking on a link now returns you to the top of the page

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,11 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+	scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 }
+  },
 })
 
 export default router
