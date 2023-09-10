5fd7c75262c54e5f31c0b2677ebcb6b9b55603ef
molul <luismorcillo@gmail.com>
2023-09-10 12:27:56 +0200

    Main page started

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
 	<div class="bg-sky-700 min-h-screen flex ">
 		<div class="mx-auto flex flex-col w-4/5 max-w-4xl">
 			<NavBar />
-			<div class="bg-white flex-1">content</div>
+			<div class="bg-white flex-1">
+				content
+				<router-view/>
+			</div>
 		</div>
 	</div>
 </template>
---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>Home</div>
+</template>
+
+<script>
+export default {
+    name: 'HomeView',
+    title: 'Inicio',
+}
+</script>
+
+<style>
+
+</style>
