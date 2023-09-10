18f53668c01fc8bf2528e5a023a8bb48d3da2d76
molul <luismorcillo@gmail.com>
2023-09-10 12:09:01 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="bg-sky-700 min-h-screen flex ">
+		<div class="mx-auto flex flex-col w-4/5 max-w-4xl">
+			<NavBar />
+			<div class="bg-white flex-1">content</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+	name: "AppComponent",
+	components: { NavBar},
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+	<nav>
+		<div class="w-full flex ">
+			<div class="flex justify-between w-full items-center p-4 ">
+				<div class="flex items-center gap-4">
+					<img src="/logo.png" />
+					<div class="font-condensed font-bold text-white text-4xl">BURGERMANIA</div>
+				</div>
+				<div class="flex space-x-8">
+					<div 
+						class="px-4 py-2 border-4 border-white rounded text-white hover:bg-white hover:text-sky-700 transform duration-300"
+						v-for="(item, index) in navigationItems" 
+						:key="index" 
+					>
+						<a :href="item.link" class="font-bold uppercase font-condensed text-2xl">{{ item.label }}</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			navigationItems: [
+			{ label: 'Carta', link: '/carta' },
+			{ label: 'Reserva', link: '/reserva' },
+			{ label: 'Pide', link: '/pide' },
+			{ label: 'Nosotros', link: '/nosotros' },
+			]
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
