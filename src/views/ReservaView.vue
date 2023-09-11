3f11e35a783546642096f0377e6d157b07952693
molul <luismorcillo@gmail.com>
2023-09-11 23:10:44 +0200

    Reservation process finished

--- src/views/ReservaView.vue
+++ src/views/ReservaView.vue
@@ -2,98 +2,130 @@
 	<div class="bg-white text-sky-700 p-4 h-full">
 		<TitleComp title="Reserva" />
 
-		<div class="text-center ">
-			¿Dónde quieres comer hoy?
-		</div>
-
-		<div class="grid grid-cols-2 gap-4 justify-between py-4 ">
-			<div 
-				class="flex "
-				v-for="(restaurant, index) in restaurants" 
-				:key="index" 
-			>
-				<RestaurantSelectComp
-					:restSelected="restaurant.selected" 
-					@select-restaurant="selectRestaurant"
-					:id="index"
-					:restPicture="restaurant.picture"
-					:name="restaurant.name"
-					:address="restaurant.address"
-				/>
-			</div>
-		</div>
-
-		<div v-if="selectedRestaurant !== -1">
-			<div class="text-center bg-sky-700 text-white py-2">
-				<div>
-					De acuerdo, vamos a reservar mesa en:
+		<!-- Reservation steps -->
+		<div v-show="status !== 'confirmed'">
+			<!-- Select restaurant -->
+			<div>
+				<div class="text-center ">
+					¿Dónde quieres comer hoy?
 				</div>
-				<div class="font-condensed font-bold text-2xl md:text-3xl uppercase">
-					{{ restaurants[selectedRestaurant].name }}
-				</div>
-			</div>
 
-			<div class="py-4 text-center flex items-center justify-center mx-auto w-full gap-4">
-				<div class="w-1/3">Elige fecha</div>
-				<div class="w-2/3">
-					<VueDatePicker 
-						v-model="date"
-						:format="format"
-						:enable-time-picker="false"
-						auto-apply
-					/>
-				</div>
-			</div>
-
-			<div class="py-4 text-center ">
-				<div class="mb-2">Elige hora</div>
-				<div class="grid grid-cols-2 gap-4 md:grid-cols-4">
+				<div class="grid grid-cols-2 gap-4 justify-between py-4 ">
 					<div 
-						v-for="(h, i) in hours" 
-						:key="i" 
-						class="border-2 border-sky-700 p-2 cursor-pointer  transition-colors duration-200" 
-						@click="selectHour(i)"
-						:class="[selectedHour === i ? 'bg-sky-700 text-white' : 'hover:text-white hover:bg-sky-600']"
-
+						class="flex "
+						v-for="(restaurant, index) in restaurants" 
+						:key="index" 
 					>
-							{{ h.hour }}
+						<RestaurantSelectComp
+							:restSelected="restaurant.selected" 
+							@select-restaurant="selectRestaurant"
+							:id="index"
+							:restPicture="restaurant.picture"
+							:name="restaurant.name"
+							:address="restaurant.address"
+						/>
 					</div>
 				</div>
 			</div>
 
-			<div v-if="selectedHour !== -1" class="text-center py-6 space-y-4">
-				<div>
-					<div class="font-condensed font-bold uppercase text-2xl">
-						Estos son los datos de tu reserva:
+			<!-- Select date and time -->
+			<div ref="selectDataTime">
+				<div v-show="selectedRestaurant !== -1">
+					<div class="text-center bg-sky-700 text-white py-2">
+						<div>
+							De acuerdo, vamos a reservar mesa en:
+						</div>
+						<div class="font-condensed font-bold text-2xl md:text-3xl uppercase">
+							{{ selectedRestaurant !== -1 && restaurants[selectedRestaurant].name }}
+						</div>
 					</div>
 
-					<div class="">
-						<span class="font-bold">Restaurante: </span>
-						{{ restaurants[selectedRestaurant].name }}
+					<div class="py-4 text-center flex items-center justify-center mx-auto w-full gap-4">
+						<div class="w-1/3">Elige fecha</div>
+						<div class="w-2/3">
+							<VueDatePicker 
+								v-model="date"
+								:format="format"
+								:enable-time-picker="false"
+								auto-apply
+							/>
+						</div>
 					</div>
 
-					<div class="">
-						<span class="font-bold">Fecha:</span> 
-						{{  date.toLocaleDateString() }}
+					<div class="py-4 text-center ">
+						<div class="mb-2">Elige hora</div>
+						<div class="grid grid-cols-2 gap-4 md:grid-cols-4">
+							<div 
+								v-for="(h, i) in hours" 
+								:key="i" 
+								class="border-2 border-sky-700 p-2 cursor-pointer  transition-colors duration-200" 
+								@click="selectHour(i)"
+								:class="[selectedHour === i ? 'bg-sky-700 text-white' : 'hover:text-white hover:bg-sky-600']"
+
+							>
+									{{ h.hour }}
+							</div>
+						</div>
 					</div>
 
-					<div class="">
-						<span class="font-bold">Hora:</span>
-						{{ hours[selectedHour].hour }}
-					</div>
+
+
 				</div>
-				<div>
-					Para terminar, pulsa el botón de debajo para confirmar tu reserva
-				</div>
-				<button 
-					class="border-4 border-yellow-700 text-yellow-700 font-condensed text-2xl uppercase py-2 px-4 font-bold hover:bg-yellow-700 hover:text-white transition-colors duration-200"
-				>
-					Confirmar
-				</button>
 			</div>
 
+			<!-- Confirm reservation -->
+			<div ref="confirmReservation">
+				<div v-show="selectedHour !== -1" class="text-center py-6 space-y-4">
+					<div>
+						<div class="font-condensed font-bold uppercase text-2xl">
+							Estos son los datos de tu reserva:
+						</div>
+
+						<div class="">
+							<span class="font-bold">Restaurante: </span>
+							{{ selectedRestaurant !== -1 && restaurants[selectedRestaurant].name }}
+						</div>
+
+						<div class="">
+							<span class="font-bold">Fecha:</span> 
+							{{  date.toLocaleDateString() }}
+						</div>
+
+						<div class="">
+							<span class="font-bold">Hora:</span>
+							{{ selectedHour !== -1 && hours[selectedHour].hour }}
+						</div>
+					</div>
+					<div>
+						Para terminar, pulsa el botón de debajo para confirmar tu reserva
+					</div>
+					<button 
+						class="border-4 font-condensed text-2xl uppercase py-2 px-4 font-bold transition-colors duration-200 "
+						:class="status === 'confirming' ? 'border-gray-500 bg-gray-500 text-gray-300' : 'border-yellow-700 text-yellow-700 hover:bg-yellow-700 hover:text-white '"
+						@click="confirmReservation"
+					>
+						<div class="flex gap-2 items-center">
+							<div class="animate-spin" v-show="status === 'confirming'">
+								<svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path class="fill-white" opacity="0.2" fill-rule="evenodd" clip-rule="evenodd" d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" fill="#000000"/>
+									<path class="fill-white" d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z" fill="#000000"/>
+								</svg>
+							</div>
+							<div>
+								{{ status === 'confirming' ? 'Confirmando...' : 'Confirmar' }}
+							</div>
+						</div>
+					</button>
+				</div>
+			</div>
+		</div>
+
+		<div v-show="status === 'confirmed'">
+			¡Reserva confirmada! 
+			Nos vemos en el restaurante {{ selectedRestaurant !== -1 && restaurants[selectedRestaurant].name }}, el {{  date.toLocaleDateString() }} a las {{ selectedHour !== -1 && hours[selectedHour].hour }}
 
 		</div>
+
 	</div>
 </template>
 
@@ -112,6 +144,7 @@ export default {
 	},
 	data() {
 		return {
+			status: 'started',
 			date: new Date(),
 			hours: [ 
 				{ id: 1, hour: "13:00"}, 
@@ -154,6 +187,11 @@ export default {
 		selectRestaurant(id) {
 			this.restaurants.forEach ( (r) => r.id === id ? r.selected = true : r.selected = false);
 			this.selectedRestaurant = id;
+
+			setTimeout ( () => {
+				this.$refs.selectDataTime.scrollIntoView({ behavior: 'smooth' });
+			}, 200)
+
 		},
 		format (date) {
 			const day = date.getDate();
@@ -164,7 +202,20 @@ export default {
 		},
 		selectHour(id) {
 			this.selectedHour = id;
+			
+			setTimeout ( () => {
+				this.$refs.confirmReservation.scrollIntoView({ behavior: 'smooth' });
+			}, 200)
+
 			// console.log(this.hours[id].hour)
+		},
+		confirmReservation () {
+			this.status = 'confirming';
+			console.log(this.status);
+			setTimeout ( () => {
+				this.status = 'confirmed';
+				console.log(this.status);
+			}, 2500)
 		}
 		
 	}
