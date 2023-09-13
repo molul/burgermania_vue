b263ca5df206f6d241a8e268389a8ea8f567593c
molul <luismorcillo@gmail.com>
2023-09-13 09:37:18 +0200

    More styling fixes for tablet portrait

--- src/views/NosotrosView.vue
+++ src/views/NosotrosView.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="space-y-4 bg-sky-700 h-full text-sky-800 px-4 md:px-0">
-		<div class="p-0 md:p-0 space-y-4 md:space-y-0 md:grid md:grid-cols-3 md:gap-4">
+	<div class="space-y-4 bg-sky-700 h-full text-sky-800 px-4 md:px-0 flex-1">
+		<div class="p-0 md:p-0 space-y-4 md:space-y-0 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-4">
 			<div class="border-4 border-white bg-sky-600 text-white font-condensed text-3xl flex items-center justify-center font-bold p-4 text-center uppercase">
 				Acerca de nosotros
 			</div>
@@ -16,9 +16,7 @@
 					Fundado en 2023, somos un restaurante dedicado a ofrecer a nuestros clientes una experiencia gastronómica única. Nuestra historia se remonta a décadas atrás, cuando un grupo de amigos con una pasión compartida por las hamburguesas decidió abrir las puertas de este lugar especial. Desde entonces, hemos trabajado incansablemente para perfeccionar nuestras recetas y técnicas, garantizando que cada bocado que ofrezcamos sea una explosión de sabor.
 				</div>
 			</div>
-		</div>
 
-		<div class="md:p-0 space-y-4 md:space-y-0 md:grid md:grid-cols-3 md:gap-4">
 			<div class="border-4 border-white bg-white text-sky-700 p-4  text-left  flex items-center">
 				<div> 
 					Lo que nos distingue es nuestro compromiso con la calidad. Utilizamos los ingredientes más frescos y de la mejor calidad para crear nuestras hamburguesas, desde la carne de res jugosa hasta las verduras crujientes y los condimentos caseros. Cada hamburguesa se cocina a la perfección, a la parrilla, para brindarte ese sabor inigualable que te mantendrá regresando por más.
@@ -37,6 +35,7 @@
 				</div>
 			</div>
 		</div>
+
 
 		<div class="">
 			<div class="border-4 border-white bg-white text-sky-700 p-4  text-left flex items-center">
